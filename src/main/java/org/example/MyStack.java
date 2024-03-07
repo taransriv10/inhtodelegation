4c508b26015b25f164cf57531e5277cb060e3b2a
taransriv10 <taransriv10@gmail.com>
2024-03-06 19:31:47 -0500

    delegate as this

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack<E> extends ArrayList<E> {
+
+    private ArrayList<E> delegate;
+
+    public MyStack() {
+        delegate = this;
+    }
+
     public void push(E e) {
-        add(e);
+        delegate.add(e);
     }
 
     public E pop() {
         if (isEmpty()) throw new EmptyStackException();
-        E e = get(size() - 1);
-        remove(size() -1 );
+        E e = delegate.get(size() - 1);
+        delegate.remove(size() -1 );
         return e;
     }
 }
